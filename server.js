3cabdbe8be5c0fed456d301eac5c918dc814c5ed
Alison Costa <alisoncosta@globo.com>
2020-09-13 21:38:48 -0300

    commit dos verbos HTTP

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-const port = 3000;
+let port = 3000;
 app.listen(port, () => {
   console.log(`o servidor foi instanciado na porta ${port}`);
 });
+app.use(bodyParser.json());
+const users = {};
 
 app.get('/', (req, res) => {
-  res.send('Minha primeiro rota');
+  res.json({ users });
 });
 
-app.get('/sobre', (req, res) => {
-  res.send('Informações referente sobre a empresa');
+app.post('/', (req, res) => {
+  const { nome, cidade, idade } = req.body;
+  users[nome] = { cidade, idade, nome };
+  res.json({ msg: 'usuário criado com sucesso!!' });
 });
 
-app.get('/sobre/empresa', (req, res) => {
-  res.send('História da empresa');
+app.put('/:nome', (req, res) => {
+  const { nome } = req.params;
+  const { cidade, idade } = req.body;
+
+  users[nome] = { cidade, idade, nome };
+  res.json({ msg: 'usuario editado com sucesso' });
+});
+
+app.delete('/:nome', (req, res) => {
+  const { nome } = req.params;
+  delete users[nome];
+  res.json({ msg: 'usuário deletado com sucesso' });
 });
